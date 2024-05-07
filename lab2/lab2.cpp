75f3e658354b6db64d93abe7c60ceac5bdb07c87
Recefi <bataev.iv4n@yandex.ru>
2024-05-08 01:57:00 +0300

    lab2 init

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -3,36 +3,38 @@
 #include <cmath>
 
 int main(int argc, char* argv[]) {
-    std::size_t iteration_count = std::stoi(argv[1]);
-    std::string str_device(argv[2]);
+    // sin(x)cos(y)dxdy, x from 0 to 1, y from 0 to 1 ~= 0.3868223
+    constexpr double EXP_VAL = 0.3868223;
 
-    auto cpu_gpu_selector = [=](sycl::device device) {
-        if (str_device == "cpu")
+    constexpr std::size_t GROUP_SIZE = 16;
+    const std::size_t intervals = std::stoi(argv[1]);
+    const double dx = 1.0 / intervals;
+    const double dy = 1.0 / intervals;
+
+    const std::string deviceStr(argv[2]);
+    auto my_selector_v = [&](sycl::device device) {
+        if (deviceStr == "cpu")
             return device.is_cpu() ? 1 : -1;
-        if (str_device == "gpu")
+        if (deviceStr == "gpu")
             return device.is_gpu() ? 1 : -1;
         else
-            return 0;
+            return (device == sycl::device(sycl::default_selector_v)) ? 1 : -1;
     };
 
-    sycl::queue queue(cpu_gpu_selector);
+    sycl::queue queue(my_selector_v);
     std::cout << "Using " << queue.get_device().get_info<sycl::info::device::name>() << std::endl;
 
-
-
-    // Compute the first n_items values in a well known sequence
-    constexpr int n_items = 16;
-    int *items = sycl::malloc_shared<int>(n_items, queue);
-    queue.parallel_for(sycl::range<1>(n_items), [items] (sycl::id<1> i) {
-        double x1 = std::pow((1.0 + sqrt(5.0))/2, i);
-        double x2 = std::pow((1.0 - sqrt(5.0))/2, i);
-        items[i] = round((x1 - x2)/sqrt(5));
-    }).wait();
-
-    for(int i = 0 ; i < n_items ; ++i) {
-        std::cout << items[i] << std::endl;
-    }
-    free(items, queue);
+    queue.submit([&](sycl::handler &cgh) {
+        cgh.parallel_for(sycl::nd_range<2>(  // global size / local size = number of work-groups (on Nvidia must be int)
+                        sycl::range<2>(intervals, intervals),  // global range, a number of work-items
+                        sycl::range<2>(GROUP_SIZE, GROUP_SIZE)),  // local range, a number of work-items in a work-group
+        [=](sycl::nd_item<2> item){
+            double x = dx * (item.get_global_id(0) + 0.5);
+            double y = dy * (item.get_global_id(1) + 0.5);
+            double val = sycl::sin(x)*sycl::cos(y)*dx*dy;
+            double sum = sycl::reduce_over_group(item.get_group(), val, std::plus<double>());
+        });
+    });
 
     return 0;
 }
